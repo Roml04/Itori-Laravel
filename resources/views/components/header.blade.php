33cf067345ba2dc69fd0248830c8b0c06d591cee
Rommel <garaisrommel04@gmail.com>
2024-12-08 22:36:38 +0800

    Initial commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,31 @@
+<head>
+    <link rel="stylesheet" href="{{asset('css/components.css')}}">
+</head>
+<header>
+    <div class="">
+        <img class="itori-logo" src="{{asset('images/hdr-itori-logo.png')}}" alt="">
+        <h1>ITORI</h1>
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="" class="">
+                    <img src="{{asset('images/icons/products-icon.svg')}}" alt="">
+                    <p class="nav-font">Products</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{asset('images/icons/login-icon.svg')}}" alt="">
+                    <p class="nav-font">Login</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{asset('images/icons/about-icon.svg')}}" alt="">
+                    <p class="nav-font">About</p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{asset('css/global.css')}}">
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+</head>
+<body>
+
+    <x-header />
+
+    <h1>This is where the home goes</h1>
+
+    <x-footer />
+
+</body>
+</html>
